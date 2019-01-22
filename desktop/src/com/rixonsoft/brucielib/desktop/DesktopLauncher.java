066d6732236956373135049664073744fa42c592
Colin Hahn <colin.hahn@sasktel.net>
2019-01-22 01:25:54 -0600

    Initial libgdx template commit

---
+++ desktop/src/com/rixonsoft/brucielib/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rixonsoft.brucielib.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rixonsoft.brucielib.TestGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TestGame(), config);
+	}
+}
